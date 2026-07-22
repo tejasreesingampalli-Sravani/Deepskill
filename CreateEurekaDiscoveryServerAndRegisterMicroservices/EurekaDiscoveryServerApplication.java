f98eeaf1905e17147c1d27ec708687dab2458cf4
tejasreesingampalli-Sravani <tejasreesingampalli@gmail.com>
2026-07-22 15:43:10 +0530

    Add files via upload

---
+++ CreateEurekaDiscoveryServerAndRegisterMicroservices/EurekaDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.eureka_discovery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryServerApplication.class, args);
+	}
+
+}
